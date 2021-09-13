f9ae5b0397c5a0b1a6dcd065df313f2b528622b0
Kevin Wilusz <kwilusz10@gmail.com>
2021-09-13 13:09:04 +0200

    gjorde whileloopen och switchlooparna för att forma menyn

--- ACME Cars/Program.cs
+++ ACME Cars/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ACME_Cars
 {
     class Program
     {
+       
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Car> carperson = new List<Car>();
+            
+            while (true)
+            {
+                Console.WriteLine("X = Exit | N = Add car | E = Edit car | D = Remove car | S = Show all cars");
+                string option = Console.ReadLine();
+                switch(option)
+                {
+                    case "x" :
+                        
+                        break;
+
+
+                    case "n":
+                       
+                        break;
+
+
+
+                    case "e":
+                       
+                        break;
+
+
+                    case "d":
+                       
+                        break;
+
+
+                    case "s":
+                        
+                        break;
+                }
+
+
+
+                Console.Clear();
+            }
+
         }
+        
+        class Car
+        {
+           public string Color;
+           public string Model;
+           public string Price;
+           public string Age;
+           public string Exhaustpipe;
+        } 
     }
 }
